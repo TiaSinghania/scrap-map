6b93cd537b3d571a4c1d6c7629f373df71786987
samanvita-singhania <samanvita.singhania@ibm.com>
2025-05-28 13:27:16 -0400

    redirected to dashboard after successful sign in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
--- src/components/auth-components.js
+++ src/components/auth-components.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { signInWithGooglePopup } from "../utils/firebase-utils"
 
 const SignIn = () => {
+    const navigate = useNavigate(); // React Router hook for navigation
     const logGoogleUser = async () => {
             const response = await signInWithGooglePopup();
             console.log(response);
+            if (response) {
+                navigate("/dashboard"); // Redirect after successful login
+            }
         }
     return (
             <div>
